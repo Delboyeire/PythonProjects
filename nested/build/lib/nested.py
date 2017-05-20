eb71abbae506ee0f9bc3a25daa9da62d3fe0b06b
derek <derekr31@hotmail.com>
2017-05-20 12:17:26 +0100

    add default switch to nested

--- nested/build/lib/nested.py
+++ nested/build/lib/nested.py
@@ -2,15 +2,15 @@
 called print_lol which print lists which may or may contain nested lists"""
 
 
-def print_lol(the_list, level=0):
+def print_lol(the_list,indent=false, level=0):
     """This function takes a positional argument called “the_list", which is any
     Python list (of, possibly, nested lists). Each data item in the provided list
     is (recursively) printed to the screen on its own line.A second arguement called 
     level is used to insert tab stops when a nested list is encountered """
     for item in the_list:
         if isinstance(item, list):
-            print_lol(item, level+1)
+            print_lol(item, indent, level+1)
         else:
-            for tab_stop in range(level):
-                print("\t", end='')
+            if indent:
+                print("\t" * level, end='')
             print(item)
